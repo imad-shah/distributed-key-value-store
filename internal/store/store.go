4b66b99eb9d7eb75a790bcec0834708835dd426e
Imad <ishah5@student.gsu.edu>
2026-07-23 21:09:25 -0500

    feat(store): add a thread-safe, in-memory, key-value store

---
+++ internal/store/store.go
@@ -0,0 +1,38 @@
+package store
+
+import (
+	"sync"
+)
+
+// Store is a thread-safe, in-memory, key-value store 
+type Store struct {
+	mu   sync.RWMutex
+	data map[string]string
+}
+
+func New() *Store {
+	return &Store{
+		data: make(map[string]string),
+	}
+}
+
+func (s *Store) Get(key string) (string, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	val, ok := s.data[key]
+	return val, ok
+}
+
+func (s *Store) Set(key, value string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data[key] = value
+}
+
+func (s *Store) Delete(key string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	_, ok := s.data[key]
+	delete(s.data, key)
+	return ok
+}
