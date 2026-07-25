5507029eead0e11807e95be76f2cc432ed5a501e
Imad <ishah5@student.gsu.edu>
2026-07-25 18:26:40 -0500

    refactor(main): main.go starts the server

--- cmd/kvstore/main.go
+++ cmd/kvstore/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"github.com/imad-shah/distributed-key-value-store/internal/server"
+)
+
 func main() {
-	// TCP server entry point 
+	// TCP server entry point
+	server.StartServer()
 }
