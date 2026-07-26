1c63a6074d7f30dd24777e3abdfcde6e8ec769bf
Imad <ishah5@student.gsu.edu>
2026-07-25 21:30:03 -0500

    refactor(main): putting it altogether
    main takes flags for creation of nodes and puts them into a new `cluster`. Then the distributed key-value store is created.

--- cmd/kvstore/main.go
+++ cmd/kvstore/main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"flag"
+	"log"
+
 	"github.com/imad-shah/distributed-key-value-store/internal/server"
+	"github.com/imad-shah/distributed-key-value-store/internal/cluster"
+	"github.com/imad-shah/distributed-key-value-store/internal/hashring"
+	"github.com/imad-shah/distributed-key-value-store/internal/store"
+)
+
+const (
+	vNodes = 256
 )
 
 func main() {
 	// TCP server entry point
-	server.StartServer()
+	id := flag.String("id", "", "this node's ID")
+	addr := flag.String("addr", ":8080", "address to listen on")
+	peersRaw := flag.String("peers", "", "comma-separated pairs of id=addr")
+	flag.Parse()
+	
+	node, err := cluster.New(*id, *addr, *peersRaw, hashring.New(vNodes))
+	if err != nil {
+		log.Fatalf("error creating cluster: %v", err)
+	}
+	kv := store.New()
+	server.StartServer(*addr, node, kv)
 }
