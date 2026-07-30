6de51352c1ae8ecb7229ccfc454bf498e19158d7
Imad <ishah5@student.gsu.edu>
2026-07-30 18:41:03 -0500

    feat(server): add internal replica commands

--- internal/server/protocol_test.go
+++ internal/server/protocol_test.go
@@ -12,28 +12,48 @@ func TestParse(t *testing.T) {
 		want    Command
 		wantErr error
 	}{
-		{"get", "GET foo", Command{Type: "GET", Key: "foo"}, nil},
-		{"getWithSpace", "GET  foo", Command{Type: "GET", Key: "foo"}, nil},
-		{"set", "SET foo bar", Command{Type: "SET", Key: "foo", Value: "bar"}, nil},
-		{"setInternalSpaces", "SET foo bar  baz", Command{Type: "SET", Key: "foo", Value: "bar  baz"}, nil},
+		{"get", "GET foo", Command{Type: CmdGet, Key: "foo"}, nil},
+		{"getWithSpace", "GET  foo", Command{Type: CmdGet, Key: "foo"}, nil},
+		{"set", "SET foo bar", Command{Type: CmdSet, Key: "foo", Value: "bar"}, nil},
+		{"setInternalSpaces", "SET foo bar  baz", Command{Type: CmdSet, Key: "foo", Value: "bar  baz"}, nil},
+		{"setMultipleValues", "SET foo bar baz", Command{Type: CmdSet, Key: "foo", Value: "bar baz"}, nil},
 		// also testing lowercase commands
-		{"delete", "delete foo", Command{Type: "DELETE", Key: "foo"}, nil},
+		{"delete", "delete foo", Command{Type: CmdDelete, Key: "foo"}, nil},
 		{"deleteExtraArg", "DELETE foo bar", Command{}, ErrWrongNumArgs},
-		{"setMultipleValues", "SET foo bar baz", Command{Type: "SET", Key: "foo", Value: "bar baz"}, nil},
 		{"empty", "", Command{}, ErrEmptyCommand},
 		{"unknown", "PING foo", Command{}, ErrUnknownCommand},
 		{"wrongNumArgsTooLittle", "GET", Command{}, ErrWrongNumArgs},
 		{"wrongNumArgsTooBig", "GET foo bar", Command{}, ErrWrongNumArgs},
+		{"replicaGet", "REPLICA_GET foo", Command{Type: CmdReplicaGet, Key: "foo"}, nil},
+		{"replicaGetMissingKey", "REPLICA_GET", Command{}, ErrWrongNumArgs},
+		{"replicaGetExtraArg", "REPLICA_GET foo bar", Command{}, ErrWrongNumArgs},
+		{"replicaSet", "REPLICA_SET foo bar", Command{Type: CmdReplicaSet, Key: "foo", Value: "bar"}, nil},
+		{"replicaSetMultipleValues", "REPLICA_SET foo bar baz", Command{Type: CmdReplicaSet, Key: "foo", Value: "bar baz"}, nil},
+		{"replicaSetMissingValue", "REPLICA_SET foo", Command{}, ErrWrongNumArgs},
+		{"replicaDelete", "REPLICA_DELETE foo", Command{Type: CmdReplicaDelete, Key: "foo"}, nil},
+		{"replicaDeleteMissingKey", "REPLICA_DELETE", Command{}, ErrWrongNumArgs},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := Parse(test.input)
+
 			if !errors.Is(err, test.wantErr) {
-				t.Fatalf("Parse(%q) error = %v, want %v", test.input, err, test.wantErr)
+				t.Fatalf(
+					"Parse(%q) error = %v, want %v",
+					test.input,
+					err,
+					test.wantErr,
+				)
 			}
+
 			if got != test.want {
-				t.Errorf("Parse(%q) got %+v, want %+v", test.input, got, test.want)
+				t.Errorf(
+					"Parse(%q) got %+v, want %+v",
+					test.input,
+					got,
+					test.want,
+				)
 			}
 		})
 	}
